7c91ff57978187f29e90db95b29d5c847792b582
Gemorroj <wapinet@gmail.com>
2013-07-06 18:16:23 +0300

    add fake test

--- tests/gmanager/GmanagerTest.php
+++ tests/gmanager/GmanagerTest.php
@@ -3,6 +3,8 @@ class GmanagerTest extends PHPUnit_Framework_TestCase
 {
     public function testIndex()
     {
-        //
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
     }
 }
